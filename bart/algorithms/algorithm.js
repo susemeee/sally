a9ea71e8a902624941e4cbe4055d8c45f464b5fc
Suho Lee <susemeee@gmail.com>
2018-06-10 23:52:32 +0900

    feat(algorithm): Add talib wrapper

--- bart/algorithms/algorithm.js
+++ bart/algorithms/algorithm.js
@@ -1,13 +1,51 @@
 
+import talib from 'talib';
+
+import util from 'util';
+import _ from 'lodash';
 
 export default class Algorithm {
   constructor() {
     this.defaultCandlePeriod = '15m';
+    this.data = {
+      open: [/* { time: Date, val: Number } */],
+      high: [/* { time: Date, val: Number } */],
+      low: [/* { time: Date, val: Number } */],
+      close: [/* { time: Date, val: Number } */],
+      volume: [/* { time: Date, val: Number } */],
+    };
   }
 
-  fillDataWith() {
+  fillData(dataToAdd) {
+    for (let [ key, data ] of Object.entries(this.data)) {
+      data.push(...dataToAdd.map(d => {
+        return { time: d.time, val: parseFloat(d[key]) };
+      }));
+    }
   }
 
-  determineSignal() {
+  get TALibData() {
+    return _.mapValues(this.data, (v) => _.orderBy(v, 't').map(v => v.val));
+  }
+
+  /**
+   * @returns Promise<TALibResult>
+   */
+  async executeTALib(name, params) {
+    try {
+      const talibResult = await util.promisify(talib.execute)({
+        name: name,
+        startIdx: 0,
+        endIdx: this.data.close.length,
+        ...params,
+      });
+      return talibResult.result;
+    } catch (err) {
+      consola.error(err);
+      throw err;
+    }
+  }
+
+  async determineSignal() {
   }
 }
--- bart/algorithms/macd.js
+++ bart/algorithms/macd.js
@@ -2,4 +2,28 @@ import Algorithm from './algorithm';
 
 export default class MACDAlgorithm extends Algorithm {
 
+  constructor() {
+    super();
+    this.macd = {
+      outMACD: [],
+      outMACDSignal: [],
+      outMACDHist: [],
+    };
+  }
+
+  async determineSignal() {
+    const { outMACD, outMACDSignal, outMACDHist } = await this.executeTALib('MACD', {
+      inReal: this.TALibData.close,
+      optInFastPeriod: 12,
+      optInSlowPeriod: 26,
+      optInSignalPeriod: 9,
+    });
+
+    this.macd.outMACD.push(outMACD);
+    this.macd.outMACDSignal.push(outMACDSignal);
+    this.macd.outMACDHist.push(outMACDHist);
+
+    // TODO:
+  }
+
 }
